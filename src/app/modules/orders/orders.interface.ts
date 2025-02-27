9346943e0d0d943a726dc1943d6d554251136884
unknown <you@example.com>
2025-02-28 02:24:18 +0600

    added order inter, model

--- src/app/modules/orders/orders.interface.ts
+++ src/app/modules/orders/orders.interface.ts
@@ -1,8 +1,20 @@
-import { Schema, model, connect, Types } from 'mongoose';
+import { Types } from 'mongoose';
 
-export type TOrders = {
-  email: string;
+export type TCustomerAddress = {
+  roadNo: string;
+  blockNo: string;
+  buildingNo: string;
+  area: string;
+  city: string;
+};
+
+export type TOrder = {
+  user: Types.ObjectId;
   product: Types.ObjectId;
   quantity: number;
   totalPrice: number;
+  orderDate: Date;
+  deliveryDate: Date;
+  orderStatus: 'Pending' | 'Shipped' | 'Delivered' | 'Cancelled';
+  customerAddress: TCustomerAddress;
 };
--- src/app/modules/orders/orders.model.ts
+++ src/app/modules/orders/orders.model.ts
@@ -1,11 +1,62 @@
 import { Schema, model } from 'mongoose';
-import { TOrders } from './orders.interface';
+import { TCustomerAddress, TOrder } from './orders.interface';
 
-const orderSchema = new Schema<TOrders>({
-  email: { type: String, required: true },
-  product: { type: Schema.Types.ObjectId, required: true },
-  quantity: { type: Number, required: true },
-  totalPrice: { type: Number, required: true },
+const customerAddressSchema = new Schema<TCustomerAddress>({
+  roadNo: {
+    type: String,
+    required: true,
+  },
+  blockNo: {
+    type: String,
+    required: true,
+  },
+  buildingNo: {
+    type: String,
+    required: true,
+  },
+  area: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
 });
 
-const Order = model<TOrders>('Order', orderSchema);
+const orderSchema = new Schema<TOrder>({
+  user: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'User',
+  },
+  product: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'Product',
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  totalPrice: {
+    type: Number,
+    required: true,
+  },
+  orderDate: {
+    type: Date,
+    default: Date.now(),
+  },
+  deliveryDate: {
+    type: Date,
+    default: () => new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+  },
+  orderStatus: {
+    type: String,
+    enum: ['Pending', 'Shipped', 'Delivered', 'Cancelled'],
+    default: 'Pending',
+  },
+  customerAddress: customerAddressSchema,
+});
+
+export const Order = model<TOrder>('Order', orderSchema);
